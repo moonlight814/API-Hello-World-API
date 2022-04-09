dbb67093580bf182cf75019c1bc6af3da1877023
Natalia <natyargumedo@hotmail.com>
2022-04-09 11:52:07 -0700

    finished the controller for language

--- models/languages_controllers.js
+++ models/languages_controllers.js
@@ -1,5 +1,21 @@
 const express= require ('express')
 const languages= express.Router()
-const Language = require('..models/language.js')
+const Language = require('..models/language.js') //data
+
+//INDEX
+languages.get('/', (req,res) => {
+    Languages.find() 
+    .then(foundLanguages => {
+        res.json(foundLanguages)
+    })
+})
+
+//SHOW
+languages.get('/:name', (req,res) => {
+    Language.findOne({name: req.params.name.toLowerCase() })
+    .then(foundLanguage => {
+        res.json(foundLanguage)
+    })
+})
 
 module.exports= languages
--- server.js
+++ server.js
@@ -18,11 +18,12 @@ app.get('/', (req, res) => {
   res.send('Welcome to the Hello World! API')
 })
 
+//Languages
+const languagesController = require('./controllers/languages_controller.js')
+app.use('/languages', languagesController)
+
 // LISTEN
 app.listen(PORT, () => {
   console.log('Greetings! From port: ', PORT);
 })
 
-//Languages
-const languagesController = require('./controllers/language_controller.js')
-app.use('/languages', languagesController)
